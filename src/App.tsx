ee3c4dbafaab34798bde222b334378358d4a711b
Your Name <shahrukh-elRed@example.com>
2025-01-06 13:06:39 +0530

    useRef hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import "./App.css";
-import { UserContextProvider } from "./components/context/UserContext";
-import { User } from "./components/context/User";
 
 function App() {
-  return (
-    <div className="App">
-      <UserContextProvider>
-        <User />
-      </UserContextProvider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
